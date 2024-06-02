7faf73323c0df4e6a4e161fada87c8f3e2fc8558
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-02 13:14:40 +0530

    Move the fetching functionality to another module.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component, StrictMode} from "react";
 import CurrencyInput from "./components/currencyInput";
+import exchangeRates from "./services/exchangeRates";
 
 class App extends Component {
 
@@ -12,39 +13,20 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    this.fetchRate(this.state.from, this.state.to)
+    this.fetchRates(this.state.from, this.state.to)
   }
 
   componentDidUpdate = (_, prevState) => {
     if(prevState.from !== this.state.from || prevState.to !== this.state.to){
-      this.fetchRate(this.state.from, this.state.to)
+      this.fetchRates(this.state.from, this.state.to)
     }
   }
 
-  fetchRate = async (base = "USD", symbol = "INR") => {
-    if(base === symbol){
-      this.setState({ rate: 1})
-    } else {
-      const url = `https://exchange-rate-api1.p.rapidapi.com/latest?base=${base}`;
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-          'X-RapidAPI-Host': 'exchange-rate-api1.p.rapidapi.com'
-        }
-      };
-
-      try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result.rates[symbol]);
-        this.setState({
-          rate: result.rates[symbol]
-        })
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  fetchRates = async (base = "USD", symbol = "INR") => {
+    const {rate} = await exchangeRates(base, symbol)
+    this.setState({
+      rate
+    })
   }
 
   render() {
---
+++ src/services/exchangeRates.js
@@ -0,0 +1,28 @@
+const exchangeRates = async (base, symbol) => {
+
+  if(base === symbol){
+    return Promise.resolve({rate: 1})
+  } else {
+    const url = `https://exchange-rate-api1.p.rapidapi.com/latest?base=${base}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'X-RapidAPI-Host': 'exchange-rate-api1.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result.rates[symbol]);
+      return Promise.resolve({
+        rate: result.rates[symbol]
+      })
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+}
+
+export default exchangeRates;
