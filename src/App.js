9d902f0ba16b286cf19af629fe1adbbcb9fd59fa
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-02 12:44:29 +0530

    Fetch live rates Using API

--- src/App.js
+++ src/App.js
@@ -11,6 +11,42 @@ class App extends Component {
     toAmt: 0
   }
 
+  componentDidMount = () => {
+    this.fetchRate(this.state.from, this.state.to)
+  }
+
+  componentDidUpdate = (_, prevState) => {
+    if(prevState.from !== this.state.from || prevState.to !== this.state.to){
+      this.fetchRate(this.state.from, this.state.to)
+    }
+  }
+
+  fetchRate = async (base = "USD", symbol = "INR") => {
+    if(base === symbol){
+      this.setState({ rate: 1})
+    } else {
+      const url = `https://exchange-rate-api1.p.rapidapi.com/latest?base=${base}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+          'X-RapidAPI-Host': 'exchange-rate-api1.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result.rates[symbol]);
+        this.setState({
+          rate: result.rates[symbol]
+        })
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
   render() {
     return (
       <div className="currency-converter">
