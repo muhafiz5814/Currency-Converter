f4faea4aa3633fec1fd8be86e46c3d2ffc065b67
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-06-02 11:49:00 +0530

    Send the value of amount prop

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     from: "USD",
     to: "INR",
     rate: 1,
-    fromAmt: 1,
-    toAmt: 1
+    fromAmt: 0,
+    toAmt: 0
   }
 
   render() {
@@ -19,11 +19,13 @@ class App extends Component {
             symbol={this.state.from} 
             selectSymbol={sym => this.setState({from: sym})}
             selectAmount={amt => this.setState({fromAmt: amt})}
+            amount={this.state.fromAmt}
           />
           <CurrencyInput 
             symbol={this.state.to} 
             selectSymbol={sym => this.setState({to: sym})}
             selectAmount={amt => this.setState({toAmt: amt})}
+            amount={parseFloat(this.state.fromAmt * this.state.rate).toFixed(2)}
           />
         </StrictMode>
       </div>
